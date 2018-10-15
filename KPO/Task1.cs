be0c743a04e3de2d7533cee5da8109cfa62a8814
dmitriy <dmitriy.shtele@gmail.com>
2018-10-15 18:38:35 +0400

    tree generator complete

--- KPO/Task1.cs
+++ KPO/Task1.cs
@@ -11,51 +11,65 @@ namespace KPO
     {
         static Random random = new Random();
         const int m = 6, N = 200, R = 100;
-        List<int> tree = new List<int>();
-        List<Tuple<int, int>> freeNodes = new List<Tuple<int, int>>();
+        List<Tuple<int, bool>> tree = new List<Tuple<int, bool>>();
+        //List<Tuple<int, int>> freeNodes = new List<Tuple<int, int>>();
         double alpha = 0.0;
 
         public void Start()
         {
-            tree.Add(0);
-            tree.Add(0);
-
+            treeGeneration:
+            tree.Add(null);
+            tree.Add(new Tuple<int, bool>(0, false));
             for (int i = 1; ; i++)
             {
                 int childQuantity = random.Next(0, 6);
                 if (childQuantity == 0)
                 {
-                    freeNodes.Add(new Tuple<int, int>(i, tree[i]));//node, parent
+                    //freeNodes.Add(new Tuple<int, int>(i, tree[i].Item1));//node, parent
                     continue;
                 }
                 for (int j = 0; j < childQuantity; j++)
-                {                    
+                {
                     if (tree.Count <= N)
-                        tree.Add(i);
+                    {
+                        tree.Add(new Tuple<int, bool>(i, true));
+                        tree[i] = new Tuple<int, bool>(tree[i].Item1, false);
+                    }
                     else
                         break;
+                    if (tree[i].Item1 == i)
+                    {
+                        tree.Clear();
+                        //freeNodes.Clear();
+                        goto treeGeneration;
+                    }
                 }
-                if (tree.Count == N)
+                if (tree.Count >= N)
                     break;
             }
 
             for (int i = 1; i < tree.Count; i++)
             {
-                Console.WriteLine(i + " - " + tree[i]);
+                Console.WriteLine(i + " - " + tree[i].Item1);
             }
 
-            if (freeNodes.Count == 0)
+            //if (freeNodes.Count == 0)
+            //{
+            //    Console.WriteLine("No free nodes");
+            //}
+            //else
             {
-                Console.WriteLine("No free nodes");
-            }
-            else
-            {
+                int freeNodesQuantity = 0;
                 Console.WriteLine("Free nodes");
-                for (int i = 0; i < freeNodes.Count; i++)
+                for (int i = 1; i < tree.Count; i++)
                 {
-                    Console.WriteLine(freeNodes[i].Item1 + " - " + freeNodes[i].Item2);
+                    if (tree[i].Item2)
+                    {
+                        Console.WriteLine(i + " - " + tree[i].Item1);
+                        freeNodesQuantity++;
+                    }
                 }
-                alpha = tree.Count / freeNodes.Count;
+                alpha = (double)tree.Count / (double)freeNodesQuantity;
                 Console.WriteLine("alpha = " + alpha);
             }
         }
